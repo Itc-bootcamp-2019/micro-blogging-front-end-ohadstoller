fd475d3afcad6dbcf8f52a8f87ecdea3e00a1b52
Ohad Stoller <ohadstoller@gmail.com>
2019-12-25 15:37:57 +0200

    cleaned app js and added a new homepage page in the pages folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import './App.css';
-import CreateTweet from './comp/CreateTweet';
+import HomePage from './pages/HomePage'
 import ProfilePage from './pages/ProfilePage';
-import MyAppContext from "./context/MyAppContext";
-import TweetList from './comp/TweetList';
-import { postTweetOnline } from "../src/lib/api";
-import { getOnlineTweets } from "../src/lib/api";
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,52 +9,16 @@ import {
   NavLink
 } from 'react-router-dom'
 
-
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tweets: [],
-      addTweet: this.handleOnSubmit.bind(this),
-      loading: true
+
     };
   };
-
-  handleOnSubmit(tweet) {
-    postTweetOnline(tweet).then(response => {
-      const tweet = response;
-      this.setState({
-        tweet: tweet, loading: false
-      });
-    });
-  };
-
-  createDefaultUser() {
-    if (localStorage.getItem('userName') == null) {
-      localStorage.setItem('userName', 'Random User')
-    };
-  }
-
-  sendGetApiRequest() {
-    getOnlineTweets().then(response => {
-      this.setState(
-        {
-          tweets: response.data.tweets
-        }
-      );
-    });
-  };
-
-  componentDidMount() {
-    document.title = `Micro-Blogging`;
-    this.createDefaultUser();
-    this.sendGetApiRequest();
-    setInterval(() => {
-      this.sendGetApiRequest();
-    }, 3000);
-  }
 
   render() {
+
     return (
       <Router>
         <>
@@ -78,20 +38,10 @@ class App extends React.Component {
           </nav>
 
           <Switch>
-            <Route exact path="/">
 
-              <MyAppContext.Provider
-                value={this.state}>
-                <div
-                  className="post-box">
-                  <CreateTweet />
-                </div>
-                <div
-                  className="feed">
-                  <TweetList />
-                </div>
-              </MyAppContext.Provider>
-
+            <Route
+              exact path="/">
+              <HomePage />
             </Route>
 
             <Route
@@ -100,7 +50,6 @@ class App extends React.Component {
             </Route>
 
           </Switch>
-
         </>
       </Router>
     );
